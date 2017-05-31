db58234b002b93e311b8c998d47a29a41546200a
Sanketh Kamath <sankethkamath99@gmail.com>
2017-05-30 23:36:00 -0700

    Added new actions.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealLabelName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard (also resign the FirstResponder Duties of the text field)
+        textField.resignFirstResponder()
+        return true
+    }
+    
+        //This method is utilized after textFieldShouldReturn and reads the written text.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealLabelName.text = textField.text
+    }
 
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        mealLabelName.text = "Default Text"
+    }
 
 }
 
